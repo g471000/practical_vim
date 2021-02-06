803f68dcc20bfde37c993693bb078d28dc77fa17
Jieun Chon <g471000@gmail.com>
2021-02-06 20:24:18 +0900

    Find and replace using f, s, ; and .

---
+++ 01_the_vim_way/3_concat.js
@@ -0,0 +1,14 @@
+From...
+car foo = "method("+argument1+", "+argument2+");
+
+To....
+car foo = "method("+argument1+", "+argument2+");
+
+f+: find +
+s + <Esc>: delete + and insert ' + '
+;:find next +
+.: do same thing
+;.: change next + and do same thing
+;.: and again
+
+car foo = "method(" + argument1 + ", " + argument2 + ");
